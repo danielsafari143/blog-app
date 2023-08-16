980e8e0c5b5e79ebda78f215b916e32d5a45e9f6
danielsafari143 <danielsafari143@gmail.com>
2023-08-16 13:48:36 +0200

    ADD UPDATE COUNTER METHOD

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
     has_one :user
+
+    def update_post_counter(name)
+      @user = User.find_by(name: name)
+      @user.postsCounter = Post.where(author_id: @user.id).count
+      @user.save
+    end
 end
