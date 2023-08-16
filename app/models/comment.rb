fd394d424dc2e1af8bf09080f3fd2a11995fb8f3
danielsafari143 <danielsafari143@gmail.com>
2023-08-16 14:27:30 +0200

    ADD UDATE COMMENT_COUNTER

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
     has_many :likes
     has_one :user
+
+    def update_comments_counter(post)
+      @post = Post.find_by(id: post)
+      @post.comments_counter = Comment.where(post_id: @post.id).count
+      @post.save
+    end
 end
