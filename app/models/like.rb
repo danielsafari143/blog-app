b3c2943f66a4aa6c297ba5eb165b481c9e5562f4
danielsafari143 <danielsafari143@gmail.com>
2023-08-16 14:39:07 +0200

    ADD UPDATE_LIKES_COUNTER METHOD

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
-class Likes < ApplicationRecord
-    belongs_to :users
-    belongs_to :comments
+class Like < ApplicationRecord
+    has_one :users
+    has_one :comments
+
+    def update_likes_counter(post)
+      @post = Post.find_by(id: post)
+      @post.likes_counter = Like.where(post_id: @post.id).count
+      @post.save
+    end
 end
