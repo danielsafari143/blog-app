5df0768ee21e4b7abfd8e9597f530ad331a3dbec
danielsafari143 <danielsafari143@gmail.com>
2023-08-16 12:03:17 +0200

    REAFACTOR

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_111534) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "Bio"
+    t.string "bio"
     t.integer "postsCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -57,5 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_111534) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id", name: "author"
 end
