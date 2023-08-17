9274919d1986737589b5c12432c5c5ac81832108
danielsafari143 <danielsafari143@gmail.com>
2023-08-17 13:54:32 +0200

    REFACTOR

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ApplicationRecord
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :update_posts_counter
 
-
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
