5b60d7159e47868e45ef1f55ba5df421ace95bc9
danielsafari143 <danielsafari143@gmail.com>
2023-08-22 19:24:01 +0200

    UPDATE TESTS

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 describe "users controller" do
     describe "get index" do
-        it "returns http succes /users" do
-            get '/users'
+        it "returns http succes" do
+            get '/users/23/posts'
             expect(response).to have_http_status(:success)
-            expect(request.body).to(eq('Daniel'))
         end
 
-        it "return http success for /users/:id" do
-            get '/users/21'
+        it "return http success for /users/:user_id/posts/:id" do
+            get '/users/2/posts/21'
             expect(response).to have_http_status(:success)
-            expect(request.body).to(eq('La rose'))
         end
     end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 
 describe "users controller" do
     describe "get index" do
-        it "returns http succes" do
-            get '/users/23/posts'
+        it "returns http succes /users" do
+            get '/users'
             expect(response).to have_http_status(:success)
         end
 
-        it "return http success for /users/:user_id/posts/:id" do
-            get '/users/2/posts/21'
+        it "shoud return the correct label" do
+            get '/users'
+            expect(response.body).to include('Here is a list of users')
+        end
+
+        it "return http success for /users/:id" do
+            get '/users/21'
             expect(response).to have_http_status(:success)
+        end
+
+        it "shoud return the correct label" do
+            get '/users/21'
+            expect(response.body).to include('Here is a user for a given userId')
         end
     end
 end
