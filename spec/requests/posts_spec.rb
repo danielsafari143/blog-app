ee5ed6f40ea65a486e9c8511613730f41dade911
danielsafari143 <danielsafari143@gmail.com>
2023-08-22 20:04:10 +0200

    UPDATE TESTS

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-describe "users controller" do
+describe "POSTS CONTROLLER" do
     describe "get index" do
         it "returns http succes" do
             get '/users/23/posts'
             expect(response).to have_http_status(:success)
+            expect(response.body).to include('Here is a list of posts for a given user')
         end
 
         it "return http success for /users/:user_id/posts/:id" do
             get '/users/2/posts/21'
             expect(response).to have_http_status(:success)
+            expect(response.body).to include('Get a post for a given postId')
         end
     end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,25 +1,29 @@
 require 'rails_helper'
 
-describe "users controller" do
+describe "USERS CONTROLLER" do
     describe "get index" do
         it "returns http succes /users" do
             get '/users'
             expect(response).to have_http_status(:success)
-        end
-
-        it "shoud return the correct label" do
-            get '/users'
+            expect(response).to render_template('index')
             expect(response.body).to include('Here is a list of users')
         end
+
+        it "return the correct template" do
+            get '/users'
+            expect(response).to render_template('index')
+        end
+
 
         it "return http success for /users/:id" do
             get '/users/21'
             expect(response).to have_http_status(:success)
+            expect(response.body).to include('Here is a user for a given userId')
         end
 
-        it "shoud return the correct label" do
+        it "return the correct template" do
             get '/users/21'
-            expect(response.body).to include('Here is a user for a given userId')
+            expect(response).to render_template('show')
         end
     end
 end
