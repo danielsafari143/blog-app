b7e14ae2e2c49daca0707972c69fbb7f2fc7af2a
danielsafari143 <danielsafari143@gmail.com>
2023-08-25 00:58:21 +0200

    ADD CHANGES

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    public def current_user
+      @user = User.find_by(id:1)
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,16 @@ class PostsController < ApplicationController
     @post = User.find_by(id: params['user_id']).posts.find_by(id: params['id'])
     @comment = Comment.where(post_id: @post)
   end
+
+  def new
+    @user = self
+  end
+
+  def create
+    @values = params[:post]
+    @post= Post.new(title: @values['title'] , text: @values['text'] , author:User.find_by(id:params['user_id']))
+    if @post.save
+      redirect_to "/users/#{params['user_id']}"
+    end
+  end
 end
