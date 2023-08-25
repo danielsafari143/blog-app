5235cbd42c588eae2694e98d6dc8ecc1a464bc8b
danielsafari143 <danielsafari143@gmail.com>
2023-08-25 04:35:29 +0200

    ADD CHANGES

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts do
-      resources :comments
+      resources :comments , :likes
     end
   end  
 end
