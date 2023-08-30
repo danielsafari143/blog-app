95e4c76f78fb9fbe115fd923367c96f2e1569b82
danielsafari143 <danielsafari143@gmail.com>
2023-08-30 02:18:19 +0200

    ADD LAST TEST

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Homepage', type: :user do
+
+  fake_user = User.create(name: 'Kena', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  first_post = Post.create(author: fake_user, title: 'Hello', text: 'This is my first post')
+  second_post = Post.create(author: fake_user, title: 'Hello', text: 'This is my first post')
+  Post.create(author: fake_user, title: 'Hello', text: 'This is my first post')
+  Comment.create('text' => 'Bonjour les freres' , 'author' => fake_user , 'post' => first_post)
+  Comment.create('text' => 'Bonjour les freres' , 'author' => fake_user , 'post' => second_post)
+
   it 'has a profile picture' do
-    visit '/users/1'
+    visit "/users/#{fake_user.id}"
 
     users = page.all('.profile-picture')
     users.each do |profile_picture|
@@ -11,7 +19,7 @@ RSpec.describe 'Homepage', type: :user do
   end
 
   it 'has a bio section' do
-    visit '/users/1'
+    visit "/users/#{fake_user.id}"
 
     expect(page).to have_css('div.bio')
     expect(page).to have_content('Bio')
@@ -19,7 +27,7 @@ RSpec.describe 'Homepage', type: :user do
   end
 
   it 'shows the number of posts each user has written' do
-    visit '/users/1'
+   visit "/users/#{fake_user.id}"
 
     users = page.all('.number-of-posts')
     users.each do |number_of_posts|
@@ -28,21 +36,21 @@ RSpec.describe 'Homepage', type: :user do
   end
 
   it 'shows the number of posts' do
-    visit '/users/1'
+   visit "/users/#{fake_user.id}"
 
     users = page.find_all('.post')
     expect(users.length).to eq(3)
   end
 
   it 'shows the button that lets me view all of a user posts.' do
-    visit '/users/1'
+   visit "/users/#{fake_user.id}"
 
     users = page.find('.btn')
     expect(users).to have_content('See all posts')
   end
 
   it 'redirects to the user show page when a user is clicked' do
-    visit '/users/1'
+   visit "/users/#{fake_user.id}"
 
     if (user = page.all('.username').first)
       user.click
@@ -53,10 +61,10 @@ RSpec.describe 'Homepage', type: :user do
   end
 
   it 'redirects to the user\'s post index page when I click to see all posts' do
-    visit '/users/1'
+   visit "/users/#{fake_user.id}"
 
     click_link('See all posts')
 
-    expect(page).to have_current_path('/users/1/posts')
+    expect(page).to have_current_path("/users/#{fake_user.id}/posts")
   end
 end
