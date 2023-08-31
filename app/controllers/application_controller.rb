6c06b1b6cb3dc05dafd8825bc7fd2111661e0d01
danielsafari143 <danielsafari143@gmail.com>
2023-08-31 19:22:41 +0200

    ADD CHANGES

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  protected def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-  end
 
   def current_user
     @user = User.find_by(id: 1)
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
   end
 end
