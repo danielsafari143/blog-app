4b5405c6ac67476835dd4a3c1f2c1aafcddd4200
danielsafari143 <danielsafari143@gmail.com>
2023-08-31 03:49:43 +0200

    UPDATE ROOT

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :logins
+  devise_for :logins ,
+   path: '/', path_names: { sign_in: 'login', sign_out: 'logout'}
+
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts do
