f72e11fce98fc2d298a9b15dcb7bff6c79e70a3d
danielsafari143 <danielsafari143@gmail.com>
2023-08-31 03:25:31 +0200

    SET UP DEVISE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_095644) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_012305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_095644) do
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
+  create_table "logins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_logins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_logins_on_reset_password_token", unique: true
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -51,6 +63,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_095644) do
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
   end
 
   add_foreign_key "comments", "posts"
