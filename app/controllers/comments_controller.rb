e29b27bc136b7cc9d10278c28d98a974ac213e3c
danielsafari143 <danielsafari143@gmail.com>
2023-09-01 15:15:27 +0200

    ADD ENDPOINT FOR ALL POST COMMENTS

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,17 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   def index
-    redirect_to "/users/#{params['user_id']}/posts/#{params['post_id']}"
+    @post = Post.find_by(id: params['post_id'])
+    
+    if @post
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @post.comments }
+      end
+    else
+      render json: { error: "Post not found" }, status: :not_found
+    end
+
   end
 
   def new
