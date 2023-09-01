b4ce3fbec75c150832ffb201340df2e7a26f71b5
danielsafari143 <danielsafari143@gmail.com>
2023-09-01 16:07:12 +0200

    ADD CHANGES

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,15 @@
 class CommentsController < ApplicationController
-
   def index
     @post = Post.find_by(id: params['post_id'])
-    
-    if @post
-      respond_to do |format|
-        format.html # index.html.erb
-        format.json { render json: @post.comments }
-      end
-    else
-      render json: { error: "Post not found" }, status: :not_found
-    end
 
+    # if @post
+    #   respond_to do |format|
+    #     format.html # index.html.erb
+    #     format.json { render json: @post.comments }
+    #   end
+    # else
+    #   render json: { error: 'Post not found' }, status: :not_found
+    # end
   end
 
   def new
@@ -22,8 +20,9 @@ class CommentsController < ApplicationController
     @values = params[:comment]
     @comment = Comment.new(text: @values['text'], author: User.find_by(id: params['user_id']),
                            post: Post.find_by(id: params['post_id']))
-    return render json: { message: "Error" }, status: :bad_request unless @comment.save
-    render json: { message: "Created" }, status: :created
+    return render json: { message: 'Error' }, status: :bad_request unless @comment.save
+
+    render json: { message: 'Created' }, status: :created
   end
 
   def destroy
